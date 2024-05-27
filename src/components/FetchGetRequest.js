965044f4042c38d35eaff28175c058b1d55591d8
Adityaraj Chauhan <chauhanadi.9@gmail.com>
2024-05-27 15:48:53 +0530

    shabby search | TODO: better search function and form implementation

--- src/components/FetchGetRequest.js
+++ src/components/FetchGetRequest.js
@@ -5,7 +5,9 @@ const FetchGetRequest = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [searchInput, setSearchinput] = useState('');
+  const [searchNameInput, setSearchNameinput] = useState('');
+  const [searchIngredientInput, setSearchIngredientinput]=useState('');
+  const [searchInventorInput, setSearchInventorinput] = useState('');
   const [filteredData, setFilteredData] = useState([]);
   const listItems = data.map((element) => {
     return (
@@ -42,27 +44,40 @@ const FetchGetRequest = () => {
     };
   
     fetchData();
-  }, [searchInput]); //call api upon component loading
+  }, [searchNameInput]); //call api upon component loading
 
-  const onChangeHandler = event => {
-    setSearchinput(event.target.value);
-    setFilteredData(data.filter(item => item.name.toLowerCase().includes(searchInput.toLowerCase())));
+  const onNameChangeHandler = event => { // TODO: create a single search Form and single Event handler
+    setSearchNameinput(event.target.value); // can use Json schema form to simplify logic , avoiding package dependencies
+    setFilteredData(data.filter(item => item.name.toLowerCase().includes(searchNameInput.toLowerCase())));
  };
+ 
+//  const onIngredientChangeHandler = event => { search by ingredient name
+//   setSearchIngredientinput(event.target.value);
+//   console.log('testing ingredient call', data)
+//   setFilteredData(data.filter(item => item.ingredients.filter(i=>i.name.toLowerCase().includes(searchIngredientInput.toLowerCase()))))};
+
+// const onInventorNameChangeHandler = event => { // search by inventor name
+//   setSearchInventorinput(event.target.value);
+//   setFilteredData(data.filter(item => item.inventors.filter(i=>i.firstName.toLowerCase().includes(searchInventorInput.toLowerCase()))));
+// };
 
  const onClickHandler = event =>{
   setData(filteredData);
  }
 
  const onResetHandler = event =>{
-  setSearchinput('');
+  setData(data)
+  
  }
 
   return (<div className="container">
     <div className='filters'>
-      <input className='search-param' placeholder='Enter name of elixir to search' id='search-param' onChange={onChangeHandler}></input>
+      <input className='search-param' placeholder='search by elixir' id='search-param' onChange={onNameChangeHandler}></input>
+      {/* <input className='search-param' placeholder='search by ingredient name ' id='search-param' onChange={onIngredientChangeHandler}></input> */}
+      {/* <input className='search-param' placeholder='search by inventor name' id='search-param' onChange={onInventorNameChangeHandler}></input> */}
       <button onClick={onClickHandler}>search</button>
-      <button onClick={onResetHandler}>resest</button>
-      </div>
+      <button onClick={onResetHandler}>reset</button>
+     </div>
       {listItems}</div>);
 };
 
